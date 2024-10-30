2175387c73441c05e8bf08aabed07f8d90b50baf
Alex Serkin <zverev095@list.ru>
2024-10-30 15:34:53 +0300

    add lesson4

---
+++ src/main/java/lesson4/Run.java
@@ -0,0 +1,9 @@
+package lesson4;
+
+public class Run {
+
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.sayHallo();
+    }
+}
